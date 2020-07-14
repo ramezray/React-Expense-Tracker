57b5139bba17a8f388455010cfd3a485abdb2359
unknown <ray_ramez@yahoo.com>
2020-07-14 16:44:11 -0700

    Updates

---
+++ precache-manifest.1a8969cc94d2e7e7457a5c93a44d512b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e20fa77b5631f33be48c1caaaea0ee5",
+    "url": "/React-Expense-Tracker/index.html"
+  },
+  {
+    "revision": "e8d2b1f67f59068d32b3",
+    "url": "/React-Expense-Tracker/static/css/main.718107d2.chunk.css"
+  },
+  {
+    "revision": "33556b952ec193449c6c",
+    "url": "/React-Expense-Tracker/static/js/2.80f3d389.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Expense-Tracker/static/js/2.80f3d389.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8d2b1f67f59068d32b3",
+    "url": "/React-Expense-Tracker/static/js/main.9d1263ef.chunk.js"
+  },
+  {
+    "revision": "4a3d36301191cf91df0d",
+    "url": "/React-Expense-Tracker/static/js/runtime-main.3c221c0c.js"
+  }
+]);
